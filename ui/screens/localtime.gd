4b50d41191527df8f6a9ef7e43095b4d31ca03b4
Alex <revooms@users.noreply.github.com>
2025-07-07 15:05:19 +0200

    Rename ads screen to localtime

--- ui/screens/localtime.gd
+++ ui/screens/localtime.gd
@@ -1,13 +1,13 @@
 extends Control
 
-@onready var screen_label = %AdsLabel
+@onready var screen_label = %LocaltimeLabel
 
 var tick_counter = 0
 var tick_interval = 30  # Alle 30 Ticks
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_ui()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -17,6 +17,5 @@ func _process(_delta: float) -> void:
 		update_ui()
 
 func update_ui() -> void:
-	var today = Time.get_datetime_string_from_system()
-	var cal = GameCalendar.get_date_object(today)
-	screen_label.text = cal.datetimestring
+	var today = Time.get_time_string_from_system()
+	screen_label.text = today
